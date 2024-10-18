2912964d58ba75341acfce2e57d996833a231d2a
razmiO <thomas.cuesta31@gmail.com>
2024-10-18 16:40:26 +0200

    Add docs

--- schema/Validator/ValidatorInterface.php
+++ schema/Validator/ValidatorInterface.php
@@ -2,7 +2,18 @@
 
 namespace Schema\Validator;
 
+/**
+ * 
+ * 
+ * All Validator classes must implement this interface.
+ * 
+ * 
+ */
 interface ValidatorInterface
 {
+    /**
+     * 
+     * The validate method is called by the Schema class to validate the client data using the intrinsic rules defined in the Validator rule object.
+     */
     public function validate($value, $key);
 }
