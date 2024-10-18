1d85c7dc26ce5d1eafa2a36b3448b662b4c4c521
razmiO <thomas.cuesta31@gmail.com>
2024-10-18 17:06:26 +0200

    Add docs

--- schema/Validator/ComplexValidator.php
+++ schema/Validator/ComplexValidator.php
@@ -10,12 +10,28 @@ use Schema\Validator\ValidatorResult;
 /**
  * 
  * Class NotBlankValidator
- * @description Validates that a value is not blank - meaning not equal to a blank string, a blank array, false or null.
+ * 
+ * 
+ * Validates that a value is not blank - meaning not equal to a blank string, a blank array, false or null.
+ * 
+ * 
  * @todo Add null behavior configuration to allow or disallow null values.
+ * 
+ * 
  */
 class NotBlankValidator implements ValidatorInterface
 {
-    public function validate($value, $key)
+    /**
+     * 
+     * 
+     * 
+     * 
+     * A value is not blank if it is not equal to a blank string, a blank array, false or null like in Symphony NotBlank constraint for forms.
+     * 
+     * 
+     * 
+     */
+    public function validate(mixed $value, string $key): ValidatorResult
     {
         return !isset($value) || $value === "" || $value === false || (is_array($value) && count($value) === 0)
             ? new ValidatorResult("not_blank", "not_blank", "blank", [$key], "Value cannot be blank")
